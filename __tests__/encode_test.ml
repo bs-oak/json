ec0f42ea45b94c3165e56d0626ec0533ae5fa757
Erik Lott <mreriklott@gmail.com>
2019-06-27 12:39:56 -0400

    update tests

--- __tests__/encode_test.ml
+++ __tests__/encode_test.ml
@@ -21,5 +21,39 @@ let () = describe "Encode" (fun () ->
             in
             expect want |> toEqual got
         );    
+
+        test "encodes values to json string with space" (fun () -> 
+            let got =
+                Encode.encode 1
+                    (Encode.object' 
+                        [ ("name", Encode.string "John Doe")
+                        ; ("age", Encode.int 41)
+                        ; ("height", Encode.float 183.4)
+                        ; ("has_hair", Encode.bool true)
+                        ; ("parent_id", Encode.null)
+                        ; ("pets", Encode.array [| Encode.string "fluffy"; Encode.string "zoomer"; Encode.string "oscar" |])
+                        ; ("fav_colors", Encode.list [ Encode.string "red"; Encode.string "white" ])
+                        ])
+            in
+            let want =
+{|{
+ "name": "John Doe",
+ "age": 41,
+ "height": 183.4,
+ "has_hair": true,
+ "parent_id": null,
+ "pets": [
+  "fluffy",
+  "zoomer",
+  "oscar"
+ ],
+ "fav_colors": [
+  "red",
+  "white"
+ ]
+}|}
+            in
+            expect want |> toEqual got
+        );         
     );
 )
